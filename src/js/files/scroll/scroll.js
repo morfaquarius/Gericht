2a4808f45052877457595194b1d63fb2eee1866d
Vyacheslav Plotnikov <vyacheslavw84@gmail.com>
2023-10-09 22:56:10 +0300

    chef block added

--- src/js/files/scroll/scroll.js
+++ src/js/files/scroll/scroll.js
@@ -172,7 +172,7 @@ export function bgParallax() {
           let bgItemPosition = bgItem.getBoundingClientRect().top + scrollY;
           let bgItemHeight = bgItem.offsetHeight;
           let bgItemScrollPrc = Math.abs((bgItem.getBoundingClientRect().top - window.innerHeight) / (bgItemHeight + window.innerHeight) * 100);
-          let bgItemPositionValue = (bgItemHeight / 100 * 30) / 100 * bgItemScrollPrc;
+          let bgItemPositionValue = (bgItemHeight / 100 * 60) / 100 * bgItemScrollPrc;
 
           if (scrollY > bgItemPosition - window.innerHeight && scrollY < bgItemPosition + bgItemHeight) {
             bgItemBg.style.cssText = `transform: translate3D(0, ${bgItemPositionValue}px, 0); transition: all 0.05s ease 0s;`;
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -51,10 +51,10 @@ function initSliders() {
 
 			// Эффекты
 			// effect: 'fade',
-			// autoplay: {
-			// 	delay: 3000,
-			// 	disableOnInteraction: false,
-			// },
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
 
 			// Пагинация
 			
