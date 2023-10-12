e699496684e5415bfdd8e419bfc602fc966067d6
Vyacheslav Plotnikov <vyacheslavw84@gmail.com>
2023-10-13 01:19:00 +0300

    video block fix

--- src/js/libs/watcher.js
+++ src/js/libs/watcher.js
@@ -148,33 +148,6 @@ class ScrollWatcher {
 				entry: entry
 			}
 		}));
-      if (document.querySelector('.video-module')) {
-        if (targetElement.dataset.watch === 'video' && !targetElement.classList.contains('_init')) {
-          if (entry.isIntersecting) {
-            // Видим объект
-            targetElement.querySelector('video').play();
-          } else {
-            // Не видим объект
-            targetElement.querySelector('video').pause();
-          }
-        }
-        const videoModule = document.querySelector('.video-module');
-        videoModule.addEventListener("click", function (e) {
-          if (!videoModule.classList.contains('_init')) {
-            videoModule.classList.add('_active');
-            videoModule.classList.add('_init');
-            videoModule.querySelector('video').play();
-            videoModule.querySelector('video').muted = false;
-          } else {
-            if (videoModule.querySelector('video').paused) {
-              videoModule.querySelector('video').play();
-            } else {
-              videoModule.querySelector('video').pause();
-            }
-            videoModule.classList.toggle('_active');
-          }
-        });
-      }
 	}
 }
 // Запускаємо та додаємо в об'єкт модулів
